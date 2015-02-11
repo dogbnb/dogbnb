ba0a6f7d30f6fd8a48c503e6084ca24491b5b44c
Michael Gudowski <michaelgudowski@gmail.com>
2015-02-11 11:43:44 -0600

    Added views

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -20,4 +20,51 @@ class HomeController extends BaseController {
 		return View::make('hello');
 	}
 
+	public function createprofile()
+	{
+		return View::make('layouts.createprofile');
+	}
+	
+	public function createreservation()
+	{
+		return View::make('layouts.createreservation');
+	}
+	public function editprofile()
+	{
+		return View::make('layouts.editprofile');
+	}
+	
+	public function index()
+	{
+		return View::make('layouts.index');
+	}
+
+	public function showlogin()
+	{
+		return View::make('layouts.login');
+	}
+
+	public function doLogin(){
+		$email = Input::get('email');
+		$password = Input::get('password');
+
+		if (Auth::attempt(array('email' => $email, 'password' => $password))) {
+    		Session::flash('successMessage', "You've logged in!");
+    		return Redirect::intended('/');
+		} else {
+	   		Session::flash('errorMessage', 'Failed to log in');
+	   		// return Redirect::action(' Example : HomeController@showLogin');
+		}
+
+
+	public function search()
+	{
+		return View::make('layouts.search');
+	}
+
+	public function signup()
+	{
+		return View::make('layouts.signup');
+	}
+
 }
