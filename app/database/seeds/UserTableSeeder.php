40510d46d0c22dc6eacec95cd9de2914b68be684
Calvin Glover <calvinglover@Calvins-MacBook-Air.local>
2015-02-12 15:38:06 -0600

    Updated composer.json to include Faker require and began adding Faker to UsersTableSeeder. Not yet functional.

--- app/database/seeds/UserTableSeeder.php
+++ app/database/seeds/UserTableSeeder.php
@@ -1,5 +1,27 @@
 <?php
 
+// // Composer: "fzaninotto/faker": "v1.3.0"
+// use Faker\Factory as Faker;
+
+// class TestTableSeeder extends Seeder {
+
+//     public function run()
+//     {
+//         $faker = Faker::create();
+
+//         foreach(range(1, 10) as $index)
+//         {
+//             Test::create([
+
+//             ]);
+//         }
+//     }
+
+// }
+
+
+
+
 class UserTableSeeder extends Seeder
 {
     public function run()
