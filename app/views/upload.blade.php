a925eb9bfc63d059bb47d9d49fac8c3f4f073166
Michael Gudowski <michaelgudowski@gmail.com>
2015-02-16 14:52:34 -0600

    Made corrections to image upload. Still needs additional logic.

--- app/views/upload.blade.php
+++ app/views/upload.blade.php
@@ -4,7 +4,7 @@
 
 <h2 class="page-header">Temporary Upload Photos Page</h2>
 
-{{ Form::open(array('action' => 'HomeController@upload', 'method'=>'POST', 'files'=>true)) }}
+{{ Form::open(array('action' => 'HomeController@photoUpload', 'method'=>'POST', 'files'=>true)) }}
 {{ Form::file('images[]', array('multiple'=>true)) }}
 	{{ Form::submit('Submit') }}
    	{{ Form::close() }}
