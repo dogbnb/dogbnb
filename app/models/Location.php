32a17ff98357d2f2679d509e9d2e1d4f63b82ce6
Calvin Glover <calvinglover@Calvins-MacBook-Air.local>
2015-02-19 18:37:40 -0600

    Added validation rules arrays to Models. Not yet implemented on controllers per review.

--- app/models/Location.php
+++ app/models/Location.php
@@ -4,6 +4,17 @@ class Location extends Eloquent {
 
     protected $table = 'locations';
 
+//     public static $rules = array(
+//     'street_number' => 'required|max:100',
+//     'street_name'   => 'required|max:10000',
+//     'city'          => 'required|max:250',
+//     'state'         => 'required|max:100',
+//     'country'       => 'required|max:100',
+//     'latitude'      => 'required|max:40',
+//     'longitude'     => 'required|max:40'
+//     // description if host?
+// );
+
     public function user()
     {
         return $this->belongsTo('User');
--- app/models/Reservation.php
+++ app/models/Reservation.php
@@ -2,6 +2,14 @@
 
 class Reservation extends Eloquent {
 
+    
+//     public static $rules = array(
+//     'in_at'         => 'required|max:20',
+//     'out_at'        => 'required|max:20',
+//     'location_id'   => 'required|max:10000',
+//     'dog_id'        => 'required|max:10000'
+// );
+
     public function dog()
     {
         return $this->belongsTo('Dog');
